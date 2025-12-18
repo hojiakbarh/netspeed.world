6a99e286a79f559336d23c4ac50e0e89b9f04a83
hojiakbarh <hoshimjonovhojiakbar008gmail.com>
2025-12-18 12:35:49 +0500

    1

--- root/settings.py
+++ root/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-3$qu$cqr-q792+vx2vh)p-hn!4q4u73u)j9hpp004s%#nt=5hi
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://netspeed-world.onrender.com','netspeed.world.onrender.com']
+ALLOWED_HOSTS = ['https://netspeed-world.onrender.com','netspeed-world.onrender.com']
 
 # Application definition
 
