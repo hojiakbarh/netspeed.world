a229601259177daeb283dd29f62c52aa43aa2368
hojiakbarh <hoshimjonovhojiakbar008gmail.com>
2026-02-26 12:23:53 +0500

    remove debug logs and fix production logging

--- root/settings.py
+++ root/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-3$qu$cqr-q792+vx2vh)p-hn!4q4u73u)j9hpp004s%#nt=5hi'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
